072334c2ac36c317c6164bc31153f1f94888b711
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2024-04-03 16:26:46 -0700

    Attributes File finished

---
+++ 06_Attributes/home.js
@@ -0,0 +1,13 @@
+const editor = document.getElementById("MyElement");
+if (editor.contentEditable) {
+    editor.setAttribute("contenteditable", "false");
+} else {
+    editor.setAttribute("contenteditable", "");
+}
+
+el.dataset[machineLearning]; //Workshop
+e.dataset.machineLearning; //Workshop
+
+for (let key in el.dataset) {
+    customObject[key] = el.dataset[key];
+  }
